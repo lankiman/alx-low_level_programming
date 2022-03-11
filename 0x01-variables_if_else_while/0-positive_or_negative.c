ae3773c7cdd9a0fa6cdea971b0efec3da586bc70
lankiman <marvellousgboun@gmail.com>
2022-03-11 15:53:46 -0800

    logical statememts

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main-Determine if a random number is positive, negative or zero.
+ *
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+
+	return (0);
+}
