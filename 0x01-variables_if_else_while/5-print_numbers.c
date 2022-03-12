ac4e1a7f8f466e23db20639e96a5ec98ca59f9a7
lankiman <marvellousgboun@gmail.com>
2022-03-11 17:59:01 -0800

    C programs

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-Printing simple digits of base 10 numbers
+ *
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
