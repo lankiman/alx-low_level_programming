2a91ca3e03775b5d258a7e44ddd99e6fcccb4ba6
lankiman <marvellousgboun@gmail.com>
2022-03-11 18:14:22 -0800

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-Printing simple digits of base 10 numbers
+ *
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
