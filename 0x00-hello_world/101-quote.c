6986da62fb581c92963babc7dde567325d2a9e64
lankiman <marvellousgboun@gmail.com>
2022-03-14 13:50:31 -0700

    c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main-print quote
+ *
+ * description- print dors quote
+ *
+ * return: always 1
+ */
+
+int main(void)
+{
+	write(2,"and that peice of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
