e90245c12ab7eb18f630e19175c8d73727eb6206
lankiman <marvellousgboun@gmail.com>
2022-03-16 20:52:15 -0700

    c libraries

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ *
+ * Description: a program that prints all charactets in lower case
+ *
+ * Return: Always 0 (success]
+ *
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+
+}
